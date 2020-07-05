c7d76ee725978d55d2606067cd7afba1a7a1e906
KProgrammer <krutarthparag.74@hotmail.com>
2020-07-05 19:14:41 +0530

    Toppet 2.0
    Removed live query, vue, used only dom(faster)

--- ToppetExtension/Toppet/code.js
+++ ToppetExtension/Toppet/code.js
@@ -1,141 +1,149 @@
-//Initialization for variables
+//Keys
 const app_id = "UndlwaMHnxf9OeojE9KG18WBDUz9cf4HoiDHhZQB";
 const js_key = "EAH5Mf9usnC5NssCOW6A0ErquwBX7q2wauGMI3tP";
 const master_key = "NYTuBCBLHNW71CRiHrNZU00dcqIxMfOpieFTe00C"
-let Answers;
-let client;
-let status;
-let vm;
-let keyGlobal = [];
+
+let AnswersClass;
+
+let keyArray = [];
+
+async function checkIfAlreadyExists(question) {
+    let query = new Parse.Query(AnswersClass);
+    query.equalTo('question', question);
+    let results = await query.find();
+    if (results.length == 0) {
+        return false;
+    } else {
+        return results[0];
+    }
+
+}
 
 //Initialize Parse Connection
 function initializeStuff() {
     Parse.initialize(app_id, js_key);
     Parse.serverURL = "https://parseapi.back4app.com/";
-    Answers = Parse.Object.extend("Answers");
-    document.getElementById('status').innerHTML = "Server Connection Initialized, setting up live query"
-
+    AnswersClass = Parse.Object.extend('Answers');
 }
 
-//Initialize Live Query Connection
-function liveQueryInit() {
-    client = new Parse.LiveQueryClient({
-        applicationId: app_id,
-        serverURL: 'wss://toppet.back4app.io',
-        javascriptKey: js_key,
-        masterKey: master_key
+//SAVES THE ANSWER TO THE SERVER
+
+function saveAnswer(question, answer) {
+    let newAnswer = new AnswersClass();
+    checkIfAlreadyExists(question).then(existingElem => {
+        if (existingElem) {
+            for (let i = 0; i < keyArray.length; i++) {
+                if (keyArray[i].question == question) {
+                    keyArray.splice(i, 1);
+                    updateText()
+                }
+            }
+            existingElem.set('answer', answer);
+            existingElem.save().then((obj) => {
+                keyArray.push({
+                    question: question,
+                    answer: answer
+                })
+                updateText();
+                console.log(`%cUpdated Answer ${question}.${answer}`, "background-color:green; font-size:20px; color:white;")
+            }, error => {
+                console.log("%cUpdated Not Saved, Error below", "background-color:red; font-size:20px; color:white;")
+                console.error(error)
+            })
+        } else {
+            newAnswer.save({
+                question: question,
+                answer: answer.toUpperCase()
+            }).then(() => {
+                console.log(`%cSaved Answer ${question}.${answer}`, "background-color:green; font-size:20px; color:white;")
+                keyArray.push({
+                    question: question,
+                    answer: answer
+                })
+                updateText();
+            }, error => {
+                console.log("%cAnswer Not Saved, Error below", "background-color:red; font-size:20px; color:white;")
+                console.error(error)
+            })
+        }
     });
-    client.open();
-    document.getElementById('status').innerHTML = "Live query initialized, waiting for data"
+    //console.log(`existingElem: ${existingElem}`)
+
 }
 
-//Gets initial data and set onCreate Listener
-async function getInitialData() {
-    const query1 = new Parse.Query(Answers);
-    results = await query1.find()
+//GETS THE KEY FROM THE SERVER
+
+async function getKey() {
+    let query = new Parse.Query(AnswersClass);
+    let results = await query.find();
+    let keyArr = [];
     for (let result of results) {
-        keyGlobal.push({
+        keyArr.push({
             question: result.get('question'),
             answer: result.get('answer')
         });
     }
-    document.getElementById('status').innerHTML = "Initial Data recieved"
-    //Add onCreate Listener
-    let query = new Parse.Query(Answers);
-    let subscription = client.subscribe(query);
-
-    subscription.on('delete', obj => {
-        console.log(obj)
-        for (let i = 0; i < vm.answer_key.length; i++) {
-            if (vm.answer_key[i].question == obj.get('question') && vm.answer_key[i].answer == obj.get('answer')) {
-                vm.answer_key.splice(i, 1);
-            }
-        }
-    });
-
-
-    subscription.on('create', obj => {
-        console.log('Object Created')
-        vm.answer_key.push({
-            question: obj.get('question'),
-            answer: obj.get('answer')
-        })
-        console.log(vm.answer_key)
-    });
-
-
-    document.getElementById('status').innerHTML += ", listener set up"
+    console.log(`%cKey Retrieved`, "background-color:green; font-size:20px; color:white;")
+    keyArray = keyArr;
+    return keyArr;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DOM INTERACTION FUNCTIONS
+
+function addText(text) {
+    let list = document.getElementById('list-id');
+    list.appendChild(document.createElement('li').appendChild(document.createTextNode(text)));
+    list.appendChild(document.createElement('br'));
+}
+
+function removeText() {
+    document.getElementById('list-id').parentNode.removeChild(document.getElementById('list-id'));
+    document.getElementById('display-key').appendChild(document.createElement('ul')).setAttribute('id', 'list-id');
+}
+
+function updateText() {
+    removeText();
+    keyArray.sort((a, b) => a.question - b.question);
+    for (let i = 0; i < keyArray.length; i++) {
+        addText(`${keyArray[i].question}. ${keyArray[i].answer.toUpperCase()}`);
+    }
+}
+
+//sets listener for button
+document.getElementById('add-answer-btn').addEventListener('click', () => {
+    saveAnswer(document.getElementById('question-text-input').value, document.getElementById('answer-text-input').value);
+})
+
+//enter key reaction
+document.getElementById('question-text-input').addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById('add-answer-btn').click();
+    }
+})
+
+document.getElementById('answer-text-input').addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById('add-answer-btn').click();
+    }
+})
+
+//RUNS INT THE BEGINNING
 
 initializeStuff();
-liveQueryInit();
-getInitialData().then(main);
-
-//Vue Instance
-function main() {
-    vm = new Vue({
-        el: "#app",
-        data: {
-            app_id: app_id,
-            js_key: js_key,
-            master_key: master_key,
-            current_question: 0,
-            current_answer: "",
-            answer_key: keyGlobal
-        },
-        computed: {
-            sorted_answer_key: function () {
-                return this.answer_key.sort((a, b) => {
-                    return a.question - b.question;
-                })
-            }
-        },
-        methods: {
-            addAnswer() {
-                const answers = new Answers();
-                let query2 = new Parse.Query(Answers);
-                query2.equalTo('question', this.current_question)
-                query2.find().then(results2 => {
-                    if (results2.length != 0) {
-                        results2[0].destroy().then(obj => {
-                            console.log("successfully destroyed")
-                            /* for (let i = 0; i < this.answer_key.length; i++) {
-                                if (this.answer_key[i].question == this.current_question) {
-                                    console.log("REMOVING")
-                                    this.answer_key.splice(i, 1)
-                                }
-                            } */
-                            answers.save({
-                                question: this.current_question,
-                                answer: this.current_answer
-                            }).then(obj => {
-                                console.log("Saved")
-                                document.getElementById('status').innerHTML = "Added answer successfully"
-                            }, error => {
-                                document.getElementById('status').innerHTML = "Tatti ho gaya, error recieved, right click and click inspect and kulla ko dikhao"
-                                console.log(error)
-                            })
-                        }, error => {
-                            console.log("TATTI HO GAYA DESTROY ME")
-                            console.error(error)
-                        })
-                        document.getElementById('status').innerHTML = "Answer already there";
-                    } else {
-                        answers.save({
-                            question: this.current_question,
-                            answer: this.current_answer
-                        }).then(obj => {
-                            console.log("Saved")
-                            document.getElementById('status').innerHTML = "Added answer successfully"
-                        }, error => {
-                            document.getElementById('status').innerHTML = "Tatti ho gaya, error recieved, right click and click inspect and kulla ko dikhao"
-                            console.log(error)
-                        })
-
-                    }
-                })
-
-            }
-        }
-    });
-}
+getKey().then((key) => {
+    updateText();
+})
